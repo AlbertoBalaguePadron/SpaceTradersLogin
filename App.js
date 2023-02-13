b942c596fdc75136c06565aa048256eddaa36f21
Alberto <alberto01bp@gmail.com>
2023-02-13 16:58:20 +0000

    Feature: Se agrega dentro del App.js la funcionalidad de deslogearse

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import { StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+
+//ventanas
 import HomeScreen from "./screens/HomeScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import LogingScreen from "./screens/LogingScreen";
@@ -12,10 +14,15 @@ import * as SecureStore from "expo-secure-store";
 const STORE_TOKEN_MY_KEY = "mytoken";
 const Drawer = createDrawerNavigator();
 
-const save = async (key, value) => {
+const saveInMemory = async (key, value) => {
   await SecureStore.setItemAsync(key, value);
 };
 
+const deleteFromMemory = async (key) => {
+  await SecureStore.deleteItemAsync(key);
+};
+
+// quitar 
 const getValueFor = async (key) => {
   let result = await SecureStore.getItemAsync(key);
   if (result) {
@@ -30,8 +37,13 @@ export default function App() {
   const [userToken, setUserToken] = useState("");
 
   const storeUserToken = (newToken) => {
-    save(STORE_TOKEN_MY_KEY, newToken);
+    saveInMemory(STORE_TOKEN_MY_KEY, newToken);
     setUserToken(newToken);
+  };
+
+  const deleteStoreUserToken = () => {
+    deleteFromMemory(STORE_TOKEN_MY_KEY);
+    setUserToken("");
   };
 
   useEffect(() => {
@@ -57,6 +69,9 @@ export default function App() {
             <>
               <Drawer.Screen name="Home" component={HomeScreen} />
               <Drawer.Screen name="Profile" component={ProfileScreen} />
+              <Drawer.Screen name="Logout">
+                {() => deleteStoreUserToken()}
+              </Drawer.Screen>
             </>
           )}
         </Drawer.Navigator>
